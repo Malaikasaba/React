9747b249d54c9b53f21e8a5ae0d1405b536f28fc
Malaika Saba <malaikasaba3@gmail.com>
2023-11-07 12:26:18 +0500

    React project added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from "./Components/Navbar";
+import Slider from "./Components/Slider";
+
+
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Slider/>
+ <h1> I am Malaika Saba</h1>
+ </>
+
+  );
+}
+export default App;
+
+
